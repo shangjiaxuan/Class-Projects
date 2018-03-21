840e06c592a85a766bfdf4a729c62d5f96a0839b
shangjiaxuan <34389517+shangjiaxuan@users.noreply.github.com>
2018-03-21 22:52:33 +0800

    fixed group[] not initializing on multiple runs

--- Crossing/Colors.cpp
+++ Crossing/Colors.cpp
@@ -12,11 +12,15 @@ void Colors::find_groups() {
 }
 
 bool Colors::evaluate(int cur, int opp) {
+//	cout << "Evaluating " << opp << " against group " << cur << endl;
 	for (int i = 0; i < Crossing::possi; i++) {
+//		cout << "evaluating against " << i << endl;
 		if (this->group[i] == cur && !this->rel->map[opp][i]) {
+//			cout << "Not okay!" << endl;
 			return false;
 		}
 	}
+//	cout << "okay!" << endl;
 	return true;
 }
 
@@ -24,11 +28,13 @@ void Colors::assign(int add) {
 	for (int i = 1; i <= this->color_num; i++) {
 		if (evaluate(i, add)) {
 			group[add] = i;
+//			cout << "Assigned " << add << " to group " << i << endl;
 			return;
 		}
 	}
 	color_num++;
 	group[add] = color_num;
+//	cout << "Assigned " << add << " to group " << color_num << endl;
 	//	return;
 }
 
--- Crossing/Colors.h
+++ Crossing/Colors.h
@@ -7,11 +7,16 @@ public:
 	Colors() {
 		rel = nullptr;
 		group = nullptr;
+		color_num = 0;
 		throw std::runtime_error("UpColors: Please specify a relation to use this class!");
 	}
 	Colors(Crossing::relations& rela) {
+		color_num = 0;
 		rel = &rela;
-		group = new int[Crossing::possi];
+		group = new int[Crossing::possi]();					//用0初始化数组
+//		for (int i = 0; i < Crossing::possi; i++) {
+//			group[i] = 0;
+//		}
 	}
 	~Colors() {
 		delete[] group;
@@ -21,7 +26,7 @@ public:
 	void print_result();
 protected:
 	Crossing::relations* rel;
-	int color_num{ 0 };
+	int color_num;
 	int* group;									//和struct route对应的分组数组
 	bool evaluate(int cur, int opp);			//看某个元素能否加入目前的组
 	void assign(int add);						//给某个元素分配一个组
