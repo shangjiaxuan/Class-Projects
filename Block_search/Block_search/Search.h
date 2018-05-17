68432976da30a3416e56825116999882b5c77a3b
shangjiaxuan <34389517+shangjiaxuan@users.noreply.github.com>
2018-05-17 22:28:02 +0800

    ...

--- Block_search/Block_search/Search.h
+++ Block_search/Block_search/Search.h
@@ -1,3 +1,50 @@
 #pragma once
 
 #include "Header.h"
+
+struct index_block {
+	int* data;
+	size_t start;
+	size_t bock_size;
+	int& operator[](size_t i) {
+		if (!(i<bock_size)) {
+			throw std::runtime_error("int& index_block::operator[](size_t): index too large!");
+		}
+		return data[start + i];
+	}
+};
+
+class search {
+	search();
+	~search();
+	size_t block_num;
+	index_block* index;
+	int* data;
+};
+
+inline std::string parse_path(std::string line) {
+	std::string path;
+	std::istringstream iss(line);
+	char current;
+	iss.get(current);
+	while (current == ' ') {
+		iss.get(current);
+	}
+	iss.putback(current);
+	while (iss) {
+		iss.get(current);
+		switch (current) {
+		case '\"':
+			iss.get(current);
+			while (current != '\"') {
+				path.push_back(current);
+				iss.get(current);
+			}
+			break;
+		default:
+			path.push_back(current);
+			iss.get(current);
+		}
+	}
+	return path;
+}
